00e114517d290649e9e438bfd1e243d539aff413
Matthew Blissett <mblissett@gbif.org>
2019-11-15 20:07:33 +0100

    Move numbers across

--- keyboards/ergodash/rev1/keymaps/default/keymap.c
+++ keyboards/ergodash/rev1/keymaps/default/keymap.c
@@ -19,9 +19,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty for Dvorak
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |VOLUME|   1  |   2  |   3  |   4  |   5  |   `  |                    |   ]  |   6  |   7  |   8  |   9  |   0  |MEDIA |
+   * |VOLUME|   1  |   2  |   3  |   4  |   5  |   6  |                    |MEDIA |   7  |   8  |   9  |   0  |   -  |  =   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |   -  |                    |   =  |   Y  |   U  |   I  |   O  |   P  |  [   |
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |   `  |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  [   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * |  \   |   A  |   S  |   D  |   F  |   G  |  GUI |                    |  GUI |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT(
-        VOLUME,       KC_1,    KC_2,     KC_3,    KC_4,    KC_5,   KC_GRV,                            KC_RBRC,     KC_6,   KC_7,     KC_8,    KC_9,     KC_0,    MEDIA,
-        KC_TAB,       KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,  KC_MINS,                             KC_EQL,     KC_Y,   KC_U,     KC_I,    KC_O,     KC_P,  KC_LBRC,
+        VOLUME,       KC_1,    KC_2,     KC_3,    KC_4,    KC_5,     KC_6,                              MEDIA,     KC_7,   KC_8,     KC_9,    KC_0,  KC_MINS,   KC_EQL,
+        KC_TAB,       KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,   KC_GRV,                            KC_RBRC,     KC_Y,   KC_U,     KC_I,    KC_O,     KC_P,  KC_LBRC,
        KC_BSLS,       KC_A,    KC_S,     KC_D,    KC_F,    KC_G,  KC_LGUI,                            KC_RGUI,     KC_H,   KC_J,     KC_K,    KC_L,  KC_SCLN,  KC_QUOT,
        KC_LSFT,       KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,              KC_ESC,        KC_DEL,               KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH, SFT_CMPS,
     C(KC_PGUP), C(KC_PGDN), KC_PGUP, ALT_PGDN,          KC_BSPC, KC_LCTL, MO(_LOWER),    MO(_LOWER), CTRL_ENT,   KC_SPC,         ALT_LEFT,   KC_UP,  KC_DOWN, KC_RIGHT
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Lower
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * | Mute |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |                    |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 | Stop |
+   * | Mute |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    | Stop |  F7  |  F8  |  F9  | F10  | F11  | F12  |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * |      |      |      |      |Pscree|      |      |                    |      |      |xNUM7 |xNUM8 |xNUM9 |      |      |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
-    KC_MUTE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,  KC_F11,                         KC_F12, KC_F6,        KC_F7,      KC_F8,      KC_F9,  KC_F10, KC_MSTP,
+    KC_MUTE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,                        KC_MSTP, KC_F7,        KC_F8,      KC_F9,     KC_F10,  KC_F11,  KC_F12,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, G(KC_KP_7), G(KC_KP_8), G(KC_KP_9), XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_INS, XXXXXXX,                        XXXXXXX, XXXXXXX, G(KC_KP_4), G(KC_KP_5), G(KC_KP_6), XXXXXXX, XXXXXXX,
     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS, XXXXXXX,           XXXXXXX,     XXXXXXX,          XXXXXXX, G(KC_KP_1), G(KC_KP_2), G(KC_KP_3), XXXXXXX, KC_RSFT,
