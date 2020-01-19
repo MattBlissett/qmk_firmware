2411da23431ca62c08816ae04291de4b4732df4b
Matthew Blissett <matt@blissett.me.uk>
2020-01-19 14:14:42 +0100

    Add MEDIA_PREV_TRACK to media key.

--- keyboards/ergodash/rev1/keymaps/default/keymap.c
+++ keyboards/ergodash/rev1/keymaps/default/keymap.c
@@ -7,7 +7,8 @@ extern keymap_config_t keymap_config;
 
 enum custom_keycodes {
   VOLUME = SAFE_RANGE,
-  MEDIA,
+  MEDIA1,
+  MEDIA2,
   OS_TAB
 };
 
@@ -19,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty for Dvorak
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |VOLUME|   1  |   2  |   3  |   4  |   5  |   6  |                    |MEDIA |   7  |   8  |   9  |   0  |   -  |  =   |
+   * |VOLUME|   1  |   2  |   3  |   4  |   5  |   6  |                    |MEDIA1|   7  |   8  |   9  |   0  |   -  |  =   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |   `  |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  [   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
@@ -31,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT(
-        VOLUME,       KC_1,    KC_2,     KC_3,    KC_4,    KC_5,     KC_6,                              MEDIA,     KC_7,   KC_8,     KC_9,    KC_0,  KC_MINS,   KC_EQL,
+        VOLUME,       KC_1,    KC_2,     KC_3,    KC_4,    KC_5,     KC_6,                             MEDIA1,     KC_7,   KC_8,     KC_9,    KC_0,  KC_MINS,   KC_EQL,
         KC_TAB,       KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,   KC_GRV,                            KC_RBRC,     KC_Y,   KC_U,     KC_I,    KC_O,     KC_P,  KC_LBRC,
        KC_BSLS,       KC_A,    KC_S,     KC_D,    KC_F,    KC_G,   OS_TAB,                             KC_APP,     KC_H,   KC_J,     KC_K,    KC_L,  KC_SCLN,  KC_QUOT,
        KC_LSFT,       KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,              KC_ESC,        KC_DEL,               KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
@@ -40,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Lower
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * | Mute |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    | Stop |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+   * | Mute |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |MEDIA2|  F7  |  F8  |  F9  | F10  | F11  | F12  |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * |      |      |      |      |Pscree|      |      |                    |      |      |xNUM7 |xNUM8 |xNUM9 |      |      |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
@@ -52,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
-    KC_MUTE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,                        KC_MSTP, KC_F7,        KC_F8,      KC_F9,     KC_F10,  KC_F11,  KC_F12,
+    KC_MUTE,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         MEDIA2, KC_F7,        KC_F8,      KC_F9,     KC_F10,  KC_F11,  KC_F12,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, G(KC_KP_7), G(KC_KP_8), G(KC_KP_9), XXXXXXX, XXXXXXX,
     XXXXXXX, KC_LEFT,   KC_UP, KC_DOWN,KC_RIGHT,  KC_INS, XXXXXXX,                        KC_CAPS, XXXXXXX, G(KC_KP_4), G(KC_KP_5), G(KC_KP_6), XXXXXXX, XXXXXXX,
     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS, XXXXXXX,           XXXXXXX,     XXXXXXX,          XXXXXXX, G(KC_KP_1), G(KC_KP_2), G(KC_KP_3), XXXXXXX, KC_RSFT,
@@ -74,8 +75,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 if (timer_elapsed(key_timer) > 300) {
                     key_repeat = false;
                 }
-                key_timer = timer_read();  // start the timer
-                return false;              // return false to keep the 1 from being sent
+                key_timer = timer_read();
+                return false;
             } else {
                 // If key was held
                 if (key_repeat || timer_elapsed(key_timer) > 150) {
@@ -94,25 +95,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
 
-        case MEDIA:
+        case MEDIA1:
             other_key_pressed = true;
 
             if (record->event.pressed) {
-                key_timer = timer_read();  // start the timer
-                return false;              // return false to keep the 1 from being sent
+                key_timer = timer_read();
+                return false;
             } else {
-                // If key was held a long time
                 if (timer_elapsed(key_timer) > 500) {
+                    // if key was long-pressed
                     register_code(KC_LGUI);
                     register_code(KC_MEDIA_STOP);
                     unregister_code(KC_MEDIA_STOP);
                     unregister_code(KC_LGUI);
                     return false;
-                //} else if (timer_elapsed(key_timer) > 500) {
-                //    register_code(KC_MEDIA_STOP);
-                //    unregister_code(KC_MEDIA_STOP);
-                //    return false;
                 } else if (timer_elapsed(key_timer) > 150) {
+                    // if key was pressed "firmly"
                     register_code(KC_MEDIA_PLAY_PAUSE);
                     unregister_code(KC_MEDIA_PLAY_PAUSE);
                     return false;
@@ -120,6 +118,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     // if key was tapped
                     register_code(KC_MEDIA_NEXT_TRACK);
                     unregister_code(KC_MEDIA_NEXT_TRACK);
+                    return false;
+                }
+            }
+            break;
+
+         case MEDIA2:
+            other_key_pressed = true;
+
+            if (record->event.pressed) {
+                key_timer = timer_read();
+                return false;
+            } else {
+                if (timer_elapsed(key_timer) > 500) {
+                    // if key was long-pressed
+                    register_code(KC_LGUI);
+                    register_code(KC_DEL);
+                    unregister_code(KC_DEL);
+                    unregister_code(KC_LGUI);
+                    return false;
+                } else if (timer_elapsed(key_timer) > 150) {
+                    // if key was pressed "firmly"
+                    register_code(KC_MEDIA_STOP);
+                    unregister_code(KC_MEDIA_STOP);
+                    return false;
+                } else {
+                    // if key was tapped
+                    register_code(KC_MEDIA_PREV_TRACK);
+                    unregister_code(KC_MEDIA_PREV_TRACK);
                     return false;
                 }
             }
