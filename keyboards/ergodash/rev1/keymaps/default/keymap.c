e426659d557fd16d279e4526c2c80705862af147
Matthew Blissett <matt@blissett.me.uk>
2020-01-19 14:15:25 +0100

    Improve OS window switching to have a reverse order key.

--- keyboards/ergodash/rev1/keymaps/default/keymap.c
+++ keyboards/ergodash/rev1/keymaps/default/keymap.c
@@ -12,9 +12,11 @@ enum custom_keycodes {
   OS_TAB
 };
 
+// Mod-Tap shortcuts
 #define CTRL_ENT RCTL_T(KC_ENT)
 #define ALT_PGDN LALT_T(KC_PGDN)
 #define ALT_LEFT RALT_T(KC_LEFT)
+#define OS_CMPSE LGUI_T(KC_APP)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -34,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
         VOLUME,       KC_1,    KC_2,     KC_3,    KC_4,    KC_5,     KC_6,                             MEDIA1,     KC_7,   KC_8,     KC_9,    KC_0,  KC_MINS,   KC_EQL,
         KC_TAB,       KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,   KC_GRV,                            KC_RBRC,     KC_Y,   KC_U,     KC_I,    KC_O,     KC_P,  KC_LBRC,
-       KC_BSLS,       KC_A,    KC_S,     KC_D,    KC_F,    KC_G,   OS_TAB,                             KC_APP,     KC_H,   KC_J,     KC_K,    KC_L,  KC_SCLN,  KC_QUOT,
+       KC_BSLS,       KC_A,    KC_S,     KC_D,    KC_F,    KC_G,   OS_TAB,                           OS_CMPSE,     KC_H,   KC_J,     KC_K,    KC_L,  KC_SCLN,  KC_QUOT,
        KC_LSFT,       KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,              KC_ESC,        KC_DEL,               KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
     C(KC_PGUP), C(KC_PGDN), KC_PGUP, ALT_PGDN,          KC_BSPC, KC_LCTL, MO(_LOWER),    MO(_LOWER), CTRL_ENT,   KC_SPC,         ALT_LEFT,   KC_UP,  KC_DOWN, KC_RIGHT
   ),
@@ -163,7 +165,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     other_key_pressed = false;
                 }
             } else {
-                if (other_key_pressed || timer_elapsed(key_timer) > 250) {
+                if (timer_elapsed(key_timer) > 250) {
                     window_switching = false;
                     unregister_code(KC_LGUI);
                 } else {
@@ -171,6 +173,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     unregister_code(KC_TAB);
                     window_switching = true;
                 }
+            }
+            return false;
+            break;
+
+        case KC_G:
+            // If window switching, hold shift to switch in the opposite direction.
+            if (window_switching) {
+                if (record->event.pressed) {
+                    register_code(KC_LSFT);
+                } else {
+                    register_code(KC_TAB);
+                    unregister_code(KC_TAB);
+                    unregister_code(KC_LSFT);
+                }
+                return false;
             }
             break;
 
